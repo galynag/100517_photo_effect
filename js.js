d59497469c8041fddc3a4e20a33debd5811ef0b2
galynag <g.alenicheva@gmail.com>
2017-07-26 22:25:59 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,3 @@
-/**
- * Created by UpscaleAcademy on 10.05.2017.
- */
 document.querySelector('.test').onmousemove = function (event) {
     var img2 = document.querySelector('.img2');
     img2.style.width=event.offsetX+'px';
